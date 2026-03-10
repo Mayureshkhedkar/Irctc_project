e9547af93cd5287873b866e2d13ac57d10b007d6
Mayureshkhedkar <mayureshkhedkar05@gmail.com>
2026-03-10 12:29:57 +0530

    handled some errors and made new functions

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -12,20 +12,20 @@ import java.io.File;
 public class UserBookingService {
 
     //global user which is logged in for all the services provided iin this class
-    private User user;
+    private final User user;
 
-    private List<User> userList;
+    private final List<User> userList;
 
 
     //the texts in the json are created in a snake case and here in the entities
     // class the general notations used are of camel case so we need a object mapper
     //object mapper maps CAMAL CASE to SNAKE CASE
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
 
 
     // this is the static user path which refeers to the local database created to get all the users from
-    private static final String USERS_PATH = "D:/Irctc project/IRCTC/app/src/main/java/org/example/localDB/Users.json";
+    private static final String USERS_PATH = "src/main/java/org/example/localDB/Users.json";
 
 
     public UserBookingService(User user1) throws IOException {
@@ -35,11 +35,13 @@ public class UserBookingService {
         });
     }
 
-    public boolean loginUser(){
-        Optional<User> foundUser = userList.stream().filter(user1 -> {return user1.getName().equals(user.getName())
-                &&
-                        UserServiceUtil.checkPassword(user.getPassword(),user1}).findFirst();
-    return foundUser.isPresent();})
+    public boolean loginUser() {
+        Optional<User> foundUser = userList.stream()
+                .filter(user1 -> user1.getName().equals(user.getName())
+                        && UserServiceUtil.checkPassword(user.getPassword(), user1))
+                .findFirst();
+
+        return foundUser.isPresent();
     }
 
     public Boolean signUp(User user1){
@@ -49,6 +51,17 @@ public class UserBookingService {
         return Boolean.TRUE;
     }catch (IOException ex){
         return Boolean.FALSE;
+        }
     }
+    private void saveUserListToFile() throws IOException {
+            File usersFile= new File(UserBookingService.USERS_PATH);
+            objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBooking(){
+        user.printTicket();
+    }
+
+
 
 }
